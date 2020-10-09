56e68fcaf87993b332f8fc30b92d3390ede99c57
EJ O`Neil <ejo7220@gmail.com>
2020-10-09 17:17:05 -0400

    edited line 5 to integer

--- db/migrate/20201009202006_create_schedules.rb
+++ db/migrate/20201009202006_create_schedules.rb
@@ -2,7 +2,7 @@ class CreateSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :schedules do |t|
       t.integer :user_id
-      t.innteger :class_id
+      t.integer :class_id
       t.boolean :reminder
 
       t.timestamps
