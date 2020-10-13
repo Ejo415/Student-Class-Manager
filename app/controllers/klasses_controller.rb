6fa9c1c2c2cc385acf33fc0581d8744562128f59
EJ O`Neil <ejo7220@gmail.com>
2020-10-13 14:58:15 -0400

    basic crud

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,2 +1,44 @@
 class KlassesController < ApplicationController
+
+    
+      def index
+        @klass = Klass.all
+      end
+    
+      def show
+        @klass = Klass.find(params[:id])
+      end
+
+      def new
+        @klass = Klass.new
+      end
+    
+      def create
+        @klass = Klass.new(klass_params)
+         if @klass.save
+        #byebug
+        redirect_to klasses_path(@klass)
+         end
+      end
+
+      def edit
+        @klass = Klass.find(params[:id])
+        
+      end
+    
+      def update
+        @klass = Klass.find(params[:id])
+        @song.update(klass_params)
+        redirect_to klass_path(@klass)
+      end
+    
+      def destroy
+        klass = Klass.find(params[:id])
+      end
+
+      private
+
+      def klass_params 
+         params.require(:klass).permit(:name, :instructor, :scheduled_time)
+      end
 end
