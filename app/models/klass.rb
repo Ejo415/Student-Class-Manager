8f1239b53ff97ce038f9bbafd3aa69fe666eb3be
EJ O`Neil <ejo7220@gmail.com>
2020-10-13 14:58:03 -0400

    added validations

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -1,4 +1,7 @@
 class Klass < ApplicationRecord
     belongs_to :schedule
     has_many :users, through: :schedule
+    validates :name, presence: true
+    validates :instructor, presence: true
+    validates :scheduled_time, presence: true
 end
