1bf58e5684812d256723f83b655847ce4f6f75c8
EJ O`Neil <ejo7220@gmail.com>
2020-10-14 15:05:20 -0400

    destroy routes for logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'sessions#welcome'
   resources :klasses
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
+  delete '/session', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
