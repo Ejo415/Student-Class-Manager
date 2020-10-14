47c39972dd1eb807fbdecf7286fd8acc5f8d3205
EJ O`Neil <ejo7220@gmail.com>
2020-10-14 16:09:11 -0400

    added show method for profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,15 @@ def new
 end
 
 def create 
-    @user = User.create(params.require(:user).permit(:username,        
-        :password))
+    @user = User.create(params.require(:user).permit(:username, :password, :email, :name, :age, :skill_level))
         session[:user_id] = @user.id
         redirect_to '/welcome'
+end
+
+def show 
+    byebug
+    @user = User.find(params[:id])
+    #byebug
 end
 
 end
