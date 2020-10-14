5d85b5dd1d7096a7557a139357e1eccf0e132ca8
EJ O`Neil <ejo7220@gmail.com>
2020-10-14 15:06:08 -0400

    created destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,27 @@ class SessionsController < ApplicationController
     skip_before_action :authorized, only: [:new, :create, :welcome]
     def new
     end
+
     def login
     end
+
     def create
        @user = User.find_by(username: params[:username])
        if @user && @user.authenticate(params[:password])
-          sessions[:user_id] = @user.id
+          session[:user_id] = @user.id
           redirect_to '/welcome'
        else
           redirect_to '/login'
        end
     end
+
+    def destroy
+        session.clear
+        redirect_to '/welcome'
+    end
+
+
     def page_requires_login
     end
+
     end
