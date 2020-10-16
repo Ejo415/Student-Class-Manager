92ae26b863df9e60fb3cd7109e88b747308ea54b
EJ O`Neil <ejo7220@gmail.com>
2020-10-16 18:38:33 -0400

    built out join controller

--- app/controllers/user_klasses_controller.rb
+++ app/controllers/user_klasses_controller.rb
@@ -1,3 +1,63 @@
-class UserKlasses < ApplicationController
+class UserKlassesController < ApplicationController
+  
+    def new
+        @klass = Klass.find_by_id(params[:klass_id])
+        @uk = @klass.user_klasses.build
+    end
+
+    def create 
+        if params[:klass_id]
+            @klass = Klass.find_by_id(params[:klass_id])
+            @uk = current_user.user_klasses.build(klass_id: params[:klass_id],reminder: params[:user_klass][:reminder])
+          else 
+        @uk = UserKlass.new(uk_params)
+          end
+          #byebug
+        if @uk.save
+       redirect_to user_path(current_user)
+        end
+    end
+
+    def index
+       # byebug
+        if current_user == User.find_by_id(params[:user_id])
+        @user = User.find_by_id(params[:user_id])
+        
+        @user_klasses = @user.user_klasses
+        else
+            redirect_to '/welcome'
+        end
+    end
+
+    def show 
+        #byebug
+        @user_klasses = UserKlass.find(params[:id])
+        if current_user.id == @user_klasses.user_id
+            else
+                redirect_to '/welcome'
+            end
+       # @user_klasses = UserKlass.find(params[:id])
+       # @user = current_user
+    end
+
     
+    def destroy
+        @user_klass = UserKlass.find(params[:id])
+        if current_user.id == @user_klass.user_id
+         @user_klass.destroy
+          redirect_to user_user_klasses_path(current_user)
+        else
+          redirect_to user_user_klasses_path(current_user)
+        end
+      end
+
+    private 
+
+    def uk_params
+        params.require(:user_klass).permit(:user_id, :klass_id, :reminder) 
+    end
+   
+   
+   
+
 end
