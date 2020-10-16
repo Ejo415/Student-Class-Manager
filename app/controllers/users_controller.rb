3d1de4e2411cae6ead8604eb10860295c39b3864
EJ O`Neil <ejo7220@gmail.com>
2020-10-16 19:42:17 -0400

    refactored create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,20 @@ def new
 end
 
 def create 
-    @user = User.create(params.require(:user).permit(:username, :password, :email, :name, :age, :skill_level))
+    @user = User.new(params.require(:user).permit(:username, :password, :email, :name, :age, :skill_level))
+        if @user.save
         session[:user_id] = @user.id
         redirect_to '/welcome'
+    else
+        @errors = @user.errors.full_messages.uniq
+           render new_user_path
+        end
 end
 
 def show 
     #byebug
     @user = User.find(params[:id])
+    @klass = Klass.all
     #byebug
 end
 
