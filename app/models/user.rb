1afd0e80c57975b789983368405b7abf97406fd6
EJ O`Neil <ejo7220@gmail.com>
2020-10-16 18:56:56 -0400

    updated for new join table and added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :userklass
-    has_many :klasses, through: :userklass
+    has_many :user_klasses
+    has_many :klasses, through: :user_klasses
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    #validates :name, presence: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true
+    scope :instructors, -> { where(instructor: true) }
 end
