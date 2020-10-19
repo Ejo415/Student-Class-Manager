24c373246567ec8948806ab4bda6b687bf8a3238
EJ O`Neil <ejo7220@gmail.com>
2020-10-19 13:44:11 -0400

    commented out mailer logic, no login for gmail

--- app/controllers/user_klasses_controller.rb
+++ app/controllers/user_klasses_controller.rb
@@ -16,9 +16,9 @@ class UserKlassesController < ApplicationController
         if @uk.save
        redirect_to user_path(current_user)
         end
-        if @uk.reminder == true
-            UserMailer.reminder(current_user).deliver_now
-        end
+      #  if @uk.reminder == true
+        #    UserMailer.reminder(current_user).deliver_now
+        #end
     
     end
 
