9b948e4e1b0dd174f1af1f4ef76233190b3534e5
EJ O`Neil <ejo7220@gmail.com>
2020-10-22 14:27:08 -0400

    created instructor helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :authorized
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :instructor
 
     def current_user    
         User.find_by(id: session[:user_id])  
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
     
     def authorized
         redirect_to '/welcome' unless logged_in?
+    end
+
+    def instructor
+        current_user.instructor
     end
 
 
